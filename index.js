e4624948a9aa17f0c90f6f01db061769cde8a49f
Joao Paulo Ribeiro <jpnribeiro94@gmail.com>
2016-01-14 21:55:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var regex = /(\(.*?\))/;
+http.createServer(function (req,res) {
+  var info = {
+    ipaddress: req.headers.host,
+    language: req.headers["accept-language"].split(",")[0],
+    software: regex.exec(req.headers["user-agent"])[0].split(/\(|\)/gi).join('')
+  };
+  res.writeHead(200,{'Content-Type':'application/json'});
+  res.write(JSON.stringify(info));
+  res.end();
+}).listen(process.env.PORT || 3000,function () {
+  console.log('server on');
+});
